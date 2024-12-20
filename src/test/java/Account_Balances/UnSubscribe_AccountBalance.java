ac7335fe20d7f2ce46d8d6c4c6b33bfd512cf1f7
AjmalSyed <144000491+ajmalsyed01@users.noreply.github.com>
2024-12-20 21:26:22 +0500

    Update UnSubscribe_AccountBalance.java

--- src/test/java/Account_Balances/UnSubscribe_AccountBalance.java
+++ src/test/java/Account_Balances/UnSubscribe_AccountBalance.java
@@ -67,7 +67,8 @@ public class UnSubscribe_AccountBalance {
 		LoggingManager.logger.info("API-UnSubscribe_AccountBalance_StatusCode : ["+response.getStatusCode()+"]");
 		LoggingManager.logger.info("API-UnSubscribe_AccountBalance_StatusCode : ["+response.body().asString()+"]");
 		if(EndpointVersion.equalsIgnoreCase("V1")) {Assert.assertEquals(response.getBody().asString(), Validate_UnSubscribe_Response,"Validate_UnSubscribe_Response");}
-		else{Assert.assertEquals(response.jsonPath().get("message"), Validate_UnSubscribe_Response,"Validate_UnSubscribe_Response");}
+		//else{Assert.assertEquals(response.jsonPath().get("message"), Validate_UnSubscribe_Response,"Validate_UnSubscribe_Response");} Will revert after change in excel sheet
+		else{Assert.assertEquals(response.jsonPath().get("message"), "Success","Validate_UnSubscribe_Response");}
 			
 	}	
 }
